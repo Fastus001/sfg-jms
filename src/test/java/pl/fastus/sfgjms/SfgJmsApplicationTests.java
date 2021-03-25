04692f0bb56934fe9a9c3a3f8e694a85c6eb510d
Fastus001 <tkarczmarzyk@gmail.com>
2021-03-25 22:44:45 +0100

    first commit

---
+++ src/test/java/pl/fastus/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fastus.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
