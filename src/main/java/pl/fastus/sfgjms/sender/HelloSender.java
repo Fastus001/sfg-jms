1a09929a4c505fc1353717fa48b85b0a1d28aa06
Fastus001 <tkarczmarzyk@gmail.com>
2021-03-26 23:02:08 +0100

    458 - Sending JMS Messages.

---
+++ src/main/java/pl/fastus/sfgjms/sender/HelloSender.java
@@ -0,0 +1,35 @@
+package pl.fastus.sfgjms.sender;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pl.fastus.sfgjms.config.JmsConfig;
+import pl.fastus.sfgjms.model.HelloWorldMessage;
+
+import java.util.UUID;
+
+/**
+ * Created by Tom - 26.03.2021
+ */
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+        System.out.println("I'm sending a message");
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello World")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message Sent!");
+    }
+}
