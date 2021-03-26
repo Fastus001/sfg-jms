e7073c6cc569e1c00a50fa5c2945131e07ebc930
Fastus001 <tkarczmarzyk@gmail.com>
2021-03-26 22:53:44 +0100

    457 - Message Converter Configuration

---
+++ src/main/java/pl/fastus/sfgjms/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package pl.fastus.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by Tom - 26.03.2021
+ */
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
